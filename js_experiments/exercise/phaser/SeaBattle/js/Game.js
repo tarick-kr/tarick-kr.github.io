69ad015861f3be2471c6592c6005bd976b98b994
Sergey <tsg-kr@ukr.net>
2018-10-21 17:10:28 +0200

    555

--- js_experiments/exercise/phaser/SeaBattle/js/Game.js
+++ js_experiments/exercise/phaser/SeaBattle/js/Game.js
@@ -15,7 +15,7 @@ game.load.image('image', 'assets/backgroundSea.png');
 // create() вызывается после того, как завершится выполнение preload(). Предназначен для создания объектов, например, спрайтов, карт, коллизий и тому подобных вещей. 
 function create() { 
  // создаем наш спрайт из картинки, которую только-что загрузили и размещаем его в центре экрана
-image= game.add.sprite(0, 0, 'image');
+image= game.add.tileSprite(0, 0, 640, 360, 'image');
 // запускаем аркадную физику в игре и делаем её доступной для нашего спрайта
 game.physics.startSystem(Phaser.Physics.ARCADE);
 game.physics.enable(image, Phaser.Physics.ARCADE)
@@ -24,5 +24,5 @@ game.physics.enable(image, Phaser.Physics.ARCADE)
 // update() вызывается 60 раз в секунду. Предназначен для обновления позиций, удаления игровых объектов. В общем для всего того, что является динамической частью игры. 
 function update() { 
  // для при,мера сделаем так, чтобы наша картинка перемещалась к низу экрана. Это можно сделать задав параметр velocity.
-image.body.velocity.x = -20;
+image.tilePosition.x = -20;
 } 
