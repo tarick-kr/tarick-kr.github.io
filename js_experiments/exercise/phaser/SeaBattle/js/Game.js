96c47536ac167a8be6bbc414a5a01b64b91a03cd
Sergey <tsg-kr@ukr.net>
2018-10-21 15:57:59 +0200

    111

--- js_experiments/exercise/phaser/SeaBattle/js/Game.js
+++ js_experiments/exercise/phaser/SeaBattle/js/Game.js
@@ -1,16 +1,45 @@
-(function() {
+// (function() {
 
 
 
 
 
 
-    WebFont.load({
-        google: {
-            families: ['Roboto+Mono']
-        },
-        active: function() {
-            GameInitialize();
-        }
-    });
-})();
+//     WebFont.load({
+//         google: {
+//             families: ['Roboto+Mono']
+//         },
+//         active: function() {
+//             GameInitialize();
+//         }
+//     });
+// })();
+
+// инициализируем Phaser. В этой строке мы указываем размер окна (800x600), тип рендера (в данном случае автоматический выбор между WebGL и Canvas), название и основные функции 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update}); 
+  
+// объявляем переменную для нашего будущего спрайта 
+var image; 
+  
+// preload() содержит код для загрузки ресурсов 
+function preload() { 
+  
+// загрузим какую-нибудь картинку: первым параметром идет название картинки, а вторым путь к ней
+game.load.image('image', 'assets/backgroundSea.png');
+  
+} 
+  
+// create() вызывается после того, как завершится выполнение preload(). Предназначен для создания объектов, например, спрайтов, карт, коллизий и тому подобных вещей. 
+function create() { 
+ // создаем наш спрайт из картинки, которую только-что загрузили и размещаем его в центре экрана
+image= game.add.sprite(game.world.centerX, game.world.centerY, 'image');
+// запускаем аркадную физику в игре и делаем её доступной для нашего спрайта
+game.physics.startSystem(Phaser.Physics.ARCADE);
+game.physics.enable(image, Phaser.Physics.ARCADE)
+} 
+  
+// update() вызывается 60 раз в секунду. Предназначен для обновления позиций, удаления игровых объектов. В общем для всего того, что является динамической частью игры. 
+function update() { 
+ // для при,мера сделаем так, чтобы наша картинка перемещалась к низу экрана. Это можно сделать задав параметр velocity.
+image.body.velocity.y = 100;
+} 
