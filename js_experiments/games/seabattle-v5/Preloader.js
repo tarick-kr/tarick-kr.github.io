73ff276404dcc51b81c9fd8ae797bb2d9675e807
Sergey <tsg-kr@ukr.net>
2018-11-12 02:22:59 +0100

    v5 bubbles

--- js_experiments/games/seabattle-v5/Preloader.js
+++ js_experiments/games/seabattle-v5/Preloader.js
@@ -14,6 +14,7 @@ BattleSea.Preloader.prototype = {
 		this.titleText = this.add.image(this.world.centerX, this.world.centerY-300, 'titleText');
 		this.titleText.anchor.setTo(0.5, 0.5);
         this.load.image('titlescreen', 'images/titleBackground2.png');
+        this.load.image('bubble', 'images/bubble.png');
         this.prelodSub = this.add.image(this.world.centerX, this.world.centerY, 'prelodSubmarine');
         this.prelodSub.anchor.setTo(0.5, 0.5);
         this.load.spritesheet('titleSubmarine', 'images/submarineSprite623x404.png', 623, 404, 9);
@@ -27,19 +28,12 @@ BattleSea.Preloader.prototype = {
         this.load.image('enemyTorpedo', 'images/enemyTorpedo.png');
         this.load.spritesheet('mySub', 'images/mySub78x45.png', 78, 45, 9);
         this.load.spritesheet('enemySub', 'images/enemySub78x45.png', 78, 45, 9);
-        // this.load.image('hill', 'images/hill.png');
-        // this.load.image('sky', 'images/sky.png');
-        // this.load.atlasXML('bunny', 'images/spritesheets/bunny.png', 'images/spritesheets/bunny.xml');
-        // this.load.atlasXML('spacerock', 'images/spritesheets/SpaceRock.png', 'images/spritesheets/SpaceRock.xml');
-        // this.load.image('explosion', 'images/explosion.png');
-        // this.load.image('ghost', 'images/ghost.png');
         this.load.audio('explosion_audio', 'audio/explosion.mp3');
         this.load.audio('gameOver_audio', 'audio/gameover.wav');
         this.load.audio('shoot_audio', 'audio/shoot.wav');
         this.load.audio('game_audio', 'audio/bgm.mp3');
+        this.load.audio('select_audio', 'audio/select.mp3');
         // this.load.audio('timerBoom_audio', 'audio/timerboom.flac');
-        
-
 	},
 
 	create: function () {
--- js_experiments/games/seabattle-v5/StartMenu.js
+++ js_experiments/games/seabattle-v5/StartMenu.js
@@ -4,11 +4,17 @@ BattleSea.StartMenu = function(game) {
     this.startPrompt;
     this.ding;
     this.style;
+    this.bubblesgroup;
+    this.totalBubbles;
+    this.buildBubblesAll;
+    this.totalBubblesAll;
+    this.startMenu;
 }
 
 BattleSea.StartMenu.prototype = {
 	
 	create: function () {
+        this.startMenu = true;
         this.ding = this.add.audio('select_audio');
         
 		startBG = this.add.image(0, 0, 'titlescreen');
@@ -17,22 +23,95 @@ BattleSea.StartMenu.prototype = {
 
         titleSub = this.add.sprite(this.world.centerX, this.world.centerY-50, 'titleSubmarine');
         titleSub.anchor.setTo(0.5, 0.5);
-        titleSub.animations.add('move', [0,1,2,3,4,5,6,7,8], 40, true);
+        titleSub.animations.add('move', [0,1,2,3,4,5,6,7,8], 50, true);
         titleSub.animations.play('move');
 
 		this.style = { font: "84px Minnie", fill: "#ffffff" };
-
 		startPrompt = this.add.text(this.world.centerX-550, this.world.centerY+250,'Нажмите чтобы начать!', this.style);
 		startPrompt.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 
+		this.totalBubbles = 200;
+		this.totalBubblesAll = 50;
+		this.buildBubbles();
+		this.buildBubblesAll();
 
 	},
+
+	buildBubblesAll: function() {
+        this.bubblesAllgroup = this.add.group();
+        
+        for(var i=0; i<this.totalBubblesAll; i++) {
+            var bubble = this.bubblesgroup.create(this.rnd.integerInRange(20, this.world.width-20), this.rnd.integerInRange(0, this.world.height-50), 'bubble');
+            var scale = this.rnd.realInRange(0.1, 0.4);
+            bubble.scale.x = scale;
+            bubble.scale.y = scale;
+            bubble.anchor.x = 0.5;
+            bubble.anchor.y = 0.5;
+            this.physics.enable(bubble, Phaser.Physics.ARCADE);
+            bubble.enableBody = true;
+            bubble.body.velocity.y = this.rnd.integerInRange(-200, -100);
+            bubble.checkWorldBounds = true;
+            bubble.events.onOutOfBounds.add(this.resetBubbleYAll, this);
+        }
+	},
+
+    buildBubbles: function() {
+        this.bubblesgroup = this.add.group();
+        
+        for(var i=0; i<this.totalBubbles; i++) {
+            var bubble = this.bubblesgroup.create(this.rnd.integerInRange(100, 150), this.rnd.integerInRange(0, this.world.height-50), 'bubble');
+            var scale = this.rnd.realInRange(0.05, 0.2);
+            bubble.scale.x = scale;
+            bubble.scale.y = scale;
+            bubble.anchor.x = 0.5;
+            bubble.anchor.y = 0.5;
+            // this.bX = this.bubble.x;
+            this.physics.enable(bubble, Phaser.Physics.ARCADE);
+            bubble.enableBody = true;
+            bubble.body.velocity.y = this.rnd.integerInRange(-400, -80);
+            // bubble.body.velocity.x = this.rnd.integerInRange(-50, 50);
+            // bubble.reversBubbleX;
+            bubble.checkWorldBounds = true;
+            bubble.events.onOutOfBounds.add(this.resetBubbleY, this);
+        }
+    },
+
+    // reversBubbleX: function(bubble) {
+    // 	if(bubble.x >= 50) {
+
+    // 	}
+    // },
+
+    resetBubbleY: function(bubble) {
+        if(bubble.y < this.world.height) {
+            this.respawnBubbleY(bubble);   
+        }
+    },
+
+    resetBubbleYAll: function(bubble) {
+        if(bubble.y < this.world.height) {
+            this.respawnBubbleYAll(bubble);   
+        }
+    },
+    
+    respawnBubbleY: function(bubble) {
+    	if(this.startMenu == true){
+	        bubble.reset(this.rnd.integerInRange(100, 150), this.world.height-50);
+	        bubble.body.velocity.y = this.rnd.integerInRange(-400, -80);
+        }
+    },
+
+    respawnBubbleYAll: function(bubble) {
+    	if(this.startMenu == true){
+	        bubble.reset(this.rnd.integerInRange(20, this.world.width-20), this.world.height-50);
+	        bubble.body.velocity.y = this.rnd.integerInRange(-200, -50);
+        }
+    },
 
 	startGame: function (pointer) {
         this.ding.play();
 		this.state.start('Game');
 	}
-
 
 };
 
