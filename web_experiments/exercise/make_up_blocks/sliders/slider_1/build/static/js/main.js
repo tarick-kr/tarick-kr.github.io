f47244a0696bd19bba6f3ca0f2151ced71a9fc18
Sergey <tsg-kr@ukr.net>
2019-01-30 12:38:31 +0200

    slider_final

--- web_experiments/exercise/make_up_blocks/sliders/slider_1/build/static/js/main.js
+++ web_experiments/exercise/make_up_blocks/sliders/slider_1/build/static/js/main.js
@@ -1,50 +1,57 @@
 $(document).ready(function () {
-
   svg4everybody({});
+  slider();
+});
 
 
-  $('.js-categories-prev').slick({
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    dots: true,
-    arrows: true,
-    prevArrow: '.categories-prev__btn.categories-prev__btn--prev',
-    nextArrow: '.categories-prev__btn.categories-prev__btn--next',
-    responsive: [
-      {
-        breakpoint: 430,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+$(window).on('resize', function () {
+  slider();
+});
+
+
+
+
+var slider = function () {
+
+  var sliderElem = $('.js-categories-prev');
+
+  if ($(window).width() < 730 && !(sliderElem.hasClass('slick-initialized'))) {
+    sliderElem.slick({
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      dots: true,
+      arrows: true,
+      prevArrow: '.categories-prev__btn.categories-prev__btn--prev',
+      nextArrow: '.categories-prev__btn.categories-prev__btn--next',
+      responsive: [
+        {
+          breakpoint: 430,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 730,
+          settings: "slick",
+        },
+        {
+          breakpoint: 4000,
+          settings: "unslick"
         }
-      },
-      {
-        breakpoint: 650,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 730,
-        settings: "slick",
-      },
-      {
-        breakpoint: 4000,
-        settings: "unslick"
-      }
-    ]
-  });
+      ]
+    });
+  } else if ($(window).width() > 730 && sliderElem.hasClass('slick-initialized')) {
+    sliderElem.slick('unslick')
+  }
 
-  var trackHeight = $(".slick-track").height();
+};
 
-  $(".slick-slide").height(trackHeight * 1.05);
-
-
-})
-
-//
-// $(window).on('resize', function () {
-//
-// });
